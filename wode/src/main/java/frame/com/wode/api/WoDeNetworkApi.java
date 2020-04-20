7225d0b7e16350ea049ac03cc68530196464c9d4
herbyuanye <344494991@qq.com>
2020-04-20 10:51:21 +0800

    合并代码

--- wode/src/main/java/frame/com/wode/api/WoDeNetworkApi.java
+++ wode/src/main/java/frame/com/wode/api/WoDeNetworkApi.java
@@ -6,17 +6,26 @@ import frame.com.libnetwork_api.ApiBase;
 public class  WoDeNetworkApi extends ApiBase {
     private static volatile WoDeNetworkApi instance = null;
 
+    private ZuoYeApiServers mServers ;
     private WoDeNetworkApi() {
         super(IUrlConfig.IAppConfig.appURL);
+        mServers=  retrofit.create(ZuoYeApiServers.class) ;
     }
 
     public static WoDeNetworkApi getInstance() {
         if (instance == null) {
             synchronized (WoDeNetworkApi.class) {
                 if (instance == null) {
+                    instance = new WoDeNetworkApi() ;
                 }
             }
         }
         return instance;
     }
+
+    public void  logIn(){
+
+        ApiSubscribe();
+    }
+
 }
---
+++ wode/src/main/java/frame/com/wode/api/ZuoYeApiServers.java
@@ -0,0 +1,11 @@
+package frame.com.wode.api;
+
+import java.util.Observable;
+
+import frame.com.libnetwork_api.BaseResult;
+
+public interface ZuoYeApiServers {
+
+
+    public Observable<BaseResult<>>
+}
