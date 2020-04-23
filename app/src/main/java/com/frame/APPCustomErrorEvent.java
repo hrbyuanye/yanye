6f13ce8b7b7c2cd343ccd586131c867a53abc8c1
herbyuanye <344494991@qq.com>
2020-04-23 23:29:21 +0800

    添加应用 CRAY 的异常处理和捕获

---
+++ app/src/main/java/com/frame/APPCustomErrorEvent.java
@@ -0,0 +1,30 @@
+package com.frame;
+
+import frame.com.libcommon.crash.CustomActivityOnCrash;
+import frame.com.libnetwork_api.log.KLog;
+
+/**
+ *  必须自己定义一个 类继承 不能使用内部类的方式 因为需要序列化
+ *
+ *  该类是放到单独的APP 运行
+ */
+public class APPCustomErrorEvent
+        implements CustomActivityOnCrash.EventListener {
+
+
+    @Override
+    public void onLaunchErrorActivity() {
+        KLog.e("发生异常 启动错误的 acvity");
+    }
+
+    @Override
+    public void onRestartAppFromErrorActivity() {
+        KLog.e(" 发生异常后 ,进入重启的ACTIVITY");
+    }
+
+    @Override
+    public void onCloseAppFromErrorActivity() {
+        KLog.e("关闭应用程序");
+    }
+}
+
--- app/src/main/java/com/frame/MyApplication.java
+++ app/src/main/java/com/frame/MyApplication.java
@@ -1,6 +1,37 @@
 package com.frame;
 
+import java.security.PublicKey;
+
 import frame.com.libcommon.BaseApplication;
+import frame.com.libcommon.crash.CaocConfig;
+import frame.com.libcommon.crash.CustomActivityOnCrash;
+import frame.com.libnetwork_api.log.KLog;
+import frame.com.modle_main.ModleMainSplish;
 
 public class MyApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initCrash();
+    }
+
+    private void initCrash() {
+        CaocConfig.Builder.create()
+                .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //背景模式,开启沉浸式
+                .enabled(true) //是否启动全局异常捕获
+                .showErrorDetails(true) //是否显示错误详细信息
+                .showRestartButton(true) //是否显示重启按钮
+                .trackActivities(true) //是否跟踪Activity
+                .minTimeBetweenCrashesMs(2000) //崩溃的间隔时间(毫秒)
+                .errorDrawable(R.mipmap.ic_launcher) //错误图标
+                .restartActivity(ModleMainSplish.class) //重新启动后的activity
+//               .errorActivity(YourCustomErrorActivity.class) //崩溃后的错误activity
+//                 可以参考 DefutError Activity 进行关闭和处理 异常
+               .eventListener(new APPCustomErrorEvent()) //崩溃后的错误监听
+                .apply();
+    }
+
+
+
 }
